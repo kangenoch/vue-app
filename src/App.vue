2e07ec1d80e3623479b36870073c591a985cc9c8
enoch <enokkang@gmail.com>
2023-08-22 14:04:08 -0500

    adds edit function

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>Employees</h1>
     <employee-form @add:employee="addEmployee" />
 
-    <employee-table :employees="employees" @delete:employee="deleteEmployee" />
+    <employee-table :employees="employees" @delete:employee="deleteEmployee" @edit:employee="editEmployee" />
   </div>
 </template>
 
@@ -50,6 +50,10 @@ export default {
 
     deleteEmployee(id) {
       this.employees = this.employees.filter((employee) => employee.id !== id);
+    },
+
+    editEmployee(id, updatedEmployee) {
+      this.employees = this.employees.map((employee) => (employee.id === id ? updatedEmployee : employee));
     },
   },
 };
